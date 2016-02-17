bf72fe63a22394ad7944f08027fbcee76e76f49e
Alex Gaskill <ajgaskill08@gmail.com>
2016-02-17 06:50:14 -0800

    Corrected issue with verification of a direction.
    Changed the verification of a direction by adding one instead of
    incrementing.

--- source/TextAdventure.cpp
+++ source/TextAdventure.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <String>
 
-
-bool setMap () {
-	bool map [3][2] = {true, false, true, true, false, true};
-	
-	return (map);
-}
-
 int main() {
 	std::string answer = "non";
 	bool isNotOut = true;
 	int coordinates[2] = { 0,0 };
+	int coordinateY = 0;
+	int coordinateX = 0;
 	std::string direction = "";
+	std::string done = "";
 
-	bool map[3][2] = { setMap() };
+	bool map[3][2] = { true, false, true, true, false, true };
 
 	std::cout << "Welcome to Text Adventure!\n";
 
@@ -31,29 +27,92 @@ int main() {
 	} while (answer != "yes" && answer != "no");
 
 	while (answer == "yes" && isNotOut) {
-		if (coordinates[0] == 0 && coordinates[1] == 0)
-			std::cout << "You find yourself in a cage and your exits are:\n";
-		if (map[coordinates[0]][coordinates[1]++])
-			std::cout << "South\n";
-		if (map[coordinates[0]++][coordinates[1]])
-			std::cout << "East\n";
-			
+
 		do {
+			if (coordinates[0] == 0 && coordinates[1] == 0)
+				std::cout << "You find yourself in a cage. Your exits are:\n";
+			if (map[coordinateX][coordinateY + 1])
+				std::cout << "South\n";
+			if (map[coordinateX][coordinateY - 1])
+				std::cout << "North\n";
+			if (map[coordinateX + 1][coordinateY])
+				std::cout << "East\n";
+			if (map[coordinateX - 1][coordinateY])
+				std::cout << "West\n";
+			std::cout << "Your coordinates are: x: " << coordinateX <<" y: " << coordinateY << std::endl;
 			std::getline(std::cin, direction);
-			if (!map[coordinates[0]][coordinates[1]++] || !map[coordinates[0]++][coordinates[1]])
-				std::cout << "You can't go that way.\n Please try a different way.\n";
-		} while (!map[coordinates[0]][coordinates[1]] && !map[coordinates[0]++][coordinates[1]]);
-		if (map[coordinates[0]][coordinates[1]++]) {
-			std::cout << "you find yourself in a cordoor\n";
-			coordinates[1]++;
-		}
-		if (map[coordinates[0]++][coordinates[1]]) {
-			std::cout << "you find yourself in a cordoor\n";
+			if ((!map[coordinateX + 1][coordinateY] || !map[coordinateX][coordinateY + 1]
+				|| !map[coordinateX][coordinateY - 1]  || !map[coordinateX - 1][coordinateY]) &&
+				(direction == "North" || direction == "South" || direction == "East" || direction == "West")) {
+				std::cout << "You can't go that way. Please try a different way.\n";
+			}
+			else if (direction != "North" && direction != "South" && direction != "East" &&
+				direction != "West") {
+				std::cout << "That is not a valid Direction. Please Try again.\n";
+			}
+
+		} while ((!map[coordinateX + 1][coordinateY] || !map[coordinateX][coordinateY + 1]
+				|| !map[coordinateX][coordinateY - 1]  || !map[coordinateX - 1][coordinateY]) 
+				&& (direction != "North" && direction != "South" && direction != "East" 
+				&& direction != "West"));
+
+		if ( direction == "South") {
 			coordinates[0]++;
+			std::cout << "You find yourself in a Hallway. Your exits are:\n";
+			if (map[coordinateX][coordinateY + 1])
+				std::cout << "South\n";
+			if (map[coordinateX][coordinateY - 1])
+				std::cout << "North\n";
+			if (map[coordinateX + 1][coordinateY])
+				std::cout << "East\n";
+			if (map[coordinateX - 1][coordinateY])
+				std::cout << "West\n";
+			std::cout << "Your coordinates are: x: " << coordinateX <<" y: " << coordinateY << std::endl;
 		}
+		else if (direction == "East") {
+			coordinates[1]++;
+			std::cout << "You find yourself in a Hallway. Your exits are:\n";
+			if (map[coordinateX][coordinateY + 1])
+				std::cout << "South\n";
+			if (map[coordinateX][coordinateY - 1])
+				std::cout << "North\n";
+			if (map[coordinateX + 1][coordinateY])
+				std::cout << "East\n";
+			if (map[coordinateX - 1][coordinateY])
+				std::cout << "West\n";
+			std::cout << "Your coordinates are: x: " << coordinateX <<" y: " << coordinateY << std::endl;
+		}
+		else if (direction == "North") {
+			coordinates[0]--;
+			std::cout << "You find yourself in a Hallway. Your exits are:\n";
+			if (map[coordinateX][coordinateY + 1])
+				std::cout << "South\n";
+			if (map[coordinateX][coordinateY - 1])
+				std::cout << "North\n";
+			if (map[coordinateX + 1][coordinateY])
+				std::cout << "East\n";
+			if (map[coordinateX - 1][coordinateY])
+				std::cout << "West\n";
+			std::cout << "Your coordinates are: x: " << coordinateX <<" y: " << coordinateY << std::endl;
+		}
+		else if (direction == "West") {
+			coordinates[1]--;
+			std::cout << "You find yourself in a Hallway. Your exits are:\n";
+			if (map[coordinateX][coordinateY + 1])
+				std::cout << "South\n";
+			if (map[coordinateX][coordinateY - 1])
+				std::cout << "North\n";
+			if (map[coordinateX + 1][coordinateY])
+				std::cout << "East\n";
+			if (map[coordinateX - 1][coordinateY])
+				std::cout << "West\n";
+			std::cout << "Your coordinates are: x: " << coordinateX <<" y: " << coordinateY << std::endl;
+		}
+
 		if (coordinates[0] == 2 && coordinates[1] == 1) {
-			std::cout << "You find the exit!\n Congradulations!\n You climb out of the dungeon!\n You win!\n";
+			std::cout << "You find the exit!\n Congradulations! You climb out of the dungeon! You win!\n";
 			isNotOut = false;
+			std::getline(std::cin, done);
 		}
 	}
 
